00622c7a543c92c3b90953ec2c0d8e9a3add5816
performabi <info@performabi.com>
2025-08-20 20:49:51 +0100

    Update vite.config.js
    update typo vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // IMPORTANT: Replace 'YOUR_REPOSITORY_NAME' with the actual name of your GitHub repository
-  base: '/https://github.com/performabi/performa_website/', 
+  base: '/performa_website/', 
   plugins: [react()],
 })
