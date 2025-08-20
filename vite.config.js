c16497b45d76940e088ed80c196fa4bf0e2c3a14
performabi <info@performabi.com>
2025-08-20 21:23:19 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/performa_website/', 
+  // Using an empty string for the base path ensures all asset links are relative.
+  // This is the most reliable method for GitHub Pages.
+  base: '', 
   plugins: [react()],
 })
