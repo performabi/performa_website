c31643edea794a5861da0a735e95a9387584d00f
Performa BI <info@performabi.com>
2025-09-11 20:45:46 +0100

    fix:update vite config and deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This is the key change: using './' makes all paths relative to the index.html file.
-  base: './', 
   plugins: [react()],
+  // This is the crucial line for GitHub Pages deployment
+  base: '/performa_website/', 
 })
