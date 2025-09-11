1ad869df051b37a2dc577e13072ed6a1f9d91491
Performa BI <info@performabi.com>
2025-09-11 21:21:29 +0100

    fix: Force absolute base path in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
-   import { defineConfig } from 'vite'
+    import { defineConfig } from 'vite'
     import react from '@vitejs/plugin-react'
-    
+
     // https://vitejs.dev/config/
-    export default defineConfig(({ command }) => {
-      if (command === 'serve') {
-        // This is the configuration for the dev server (npm run dev)
-        return {
-          plugins: [react()],
-        }
-      } else {
-        // This is the configuration for the production build (npm run build)
-        return {
-          plugins: [react()],
-          base: '/performa_website/',
-        }
-      }
+    export default defineConfig({
+      plugins: [react()],
+      // This setting is NOT conditional. It will be applied every time,
+      // which is what the GitHub Actions build needs.
+      base: '/performa_website/',
     })
